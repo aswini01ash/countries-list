75b3edf7504d40071b85d110fa06426ed2ad5e87
Aswini Ashok <aswiniashok06@gmail.com>
2025-09-22 17:05:33 +0530

    UI enhancements

--- app/src/main/java/com/example/countires/presentation/screens/CountryDetailsScreen.kt
+++ app/src/main/java/com/example/countires/presentation/screens/CountryDetailsScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -58,7 +58,7 @@ fun CountryDetailScreen(
             navigationIcon = {
                 IconButton(onClick = onBackClick) {
                     Icon(
-                        Icons.Default.ArrowBack,
+                        Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = stringResource(R.string.back_button_desc)
                     )
                 }
